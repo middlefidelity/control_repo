d7f6f097d4aad36fba3c2f62465b0188cd69b8a1
middlefidelity <55301738+middlefidelity@users.noreply.github.com>
2022-01-03 16:23:39 -0500

    Update site.pp

--- manifests/site.pp
+++ manifests/site.pp
@@ -1,10 +1,2 @@
 node default {
-  file {'/root/README':
-    ensure    => file,
-    content   => 'This is a frickin readme',
-    owner     => 'root',
-  }
-  file {'/root/README':
-    owner     => 'root',
-  }
 }
